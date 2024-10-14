b5716df0ee4ba67e09b3b63e5db85615845fe80b
Harold Dinglasan <dinglasan.harold.ian.dave@gmail.com>
2024-10-14 18:53:03 +0800

    Modified NavBarAdmin and offenseTableAdmin css

--- src/component/NavBarAdmin.jsx
+++ src/component/NavBarAdmin.jsx
@@ -1,24 +1,22 @@
 
 import React from 'react';
-import { useNavigate } from 'react-router-dom';
 import logo from '../assets/logo_new.png';
 import user from '../assets/user.png';
-
 import '../styles/offenseTableAdmin.css';
 
 const NavBar = ({ handleLogout }) => {
-    const navigate = useNavigate();
-
     return (
         <nav className="nav-bar">
             <img src={logo} alt="Logo" className="rc-logo"/>
+
             <div className="nav-links">
                 <a className="nav-link" href="/admin/offense">Offense</a>
                 <a className="nav-link" href="/admin/violation">Violation</a>
                 <a className="nav-link" href="/admin/cs-list">CS Slips</a>
-                <a className="nav-link" href="#" onMouseDown={handleLogout}>Logout</a>
+                <button className="nav-link-button" onMouseDown={handleLogout}>Logout</button>
                 <img src={user} alt="profile" className="profile"/>
             </div>
+
         </nav>
     );
 };
