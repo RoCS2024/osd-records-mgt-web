777afdfce693a4cafe67dd8cba55a952148fe1cd
Harold Dinglasan <dinglasan.harold.ian.dave@gmail.com>
2024-11-09 21:57:27 +0800

    Refactor NavBar and ViolationTableAdmin styles

--- src/component/NavBar.jsx
+++ src/component/NavBar.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 
-import { useNavigate } from 'react-router-dom';
 import logo from '../assets/logo_new.png';
 import user from '../assets/user.png';
 
@@ -8,15 +7,13 @@ import '../styles/ViolationStudent.css';
 import '../styles/ViolationGuest.css';
 
 const NavBar = ({ handleLogout }) => {
-    const navigate = useNavigate();
-
     return (
         <nav className="nav-bar">
             <img src={logo} alt="Logo" className="rc-logo" />
             <div className="nav-links">
                 <a href="/student/violation">Violation</a>
                 <a href="/student/cs-slip">Cs Slips</a>
-                <a href="#" onClick={handleLogout}>Logout</a>
+                <button onClick={handleLogout} className="logout-button">Logout</button>
                 <img src={user} alt="profile" className="profile" />
             </div>
         </nav>
