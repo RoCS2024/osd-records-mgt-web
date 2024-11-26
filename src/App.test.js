c9cf74f632a55b8b205d7fb01e4764cea1adc1a9
Kate Ann I. Amulong <amulongkateann@gmail.com>
2024-11-26 19:32:59 +0800

    RCPS-165:Create Unit Test for Verification of OTP

--- src/App.test.js
+++ src/App.test.js
@@ -1,10 +1,6 @@
 import { render, screen } from '@testing-library/react';
 import { MemoryRouter } from 'react-router-dom';
 import App from './App';
-import OffensePageAdmin from './pages/OffenseTableAdmin';
-import ForgotPassword from './pages/ForgotPassword';
-import ViolationPageAdmin from './pages/ViolationTableAdmin';
-import OtpForm from './pages/OTP';
 
 
 test('renders login page', () => {
@@ -20,16 +16,5 @@ test('renders create account page', () => {
   const createAccountTextElements = screen.getAllByText(/click here/i);
   expect(createAccountTextElements.length).toBeGreaterThan(0);
 
-});
-
-test('renders OTP page', () => {
-  render(
-    <MemoryRouter>
-      <OtpForm />
-    </MemoryRouter>
-  );
-
-  const verifyOtpButton = screen.getByRole('button', { name: /verify otp/i });
-  expect(verifyOtpButton).toBeInTheDocument();
 });
 
